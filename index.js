196397760ea7b24a8d8fbf4211ae7c01df9e689b
於楚耀 <yuchuyao1@163.com>
2023-02-02 15:12:34 +0800

    测试

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const spawn = require('cross-spawn');
 
-const result = spawn.sync('npm', ['list', '-g', '-depth', '0'], { stdio: 'inherit' });
-console.log(result);
+spawn.sync('git', ['add', '.'], { stdio: 'inherit' });
 
-// echo "# automatic" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/wucyAAA/automatic.git
-// git push -u origin main
+spawn.sync('git', ['commit','-m', '测试'], { stdio: 'inherit' });
+
+spawn.sync('git', ['push'], { stdio: 'inherit' });
+
+ 
